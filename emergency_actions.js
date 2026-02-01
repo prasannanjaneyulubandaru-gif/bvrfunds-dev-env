158a46379ea866860cf0abeba9f151392983cbe3
prasannanjaneyulubandaru-gif <prasannanjaneyulu.bandaru@gmail.com>
2026-02-01 10:54:18 +0530

    Update backend URL for emergency actions

--- emergency_actions.js
+++ emergency_actions.js
@@ -3,7 +3,7 @@
 const EMERGENCY_CONFIG = {
     backendUrl: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
         ? 'http://localhost:5000' 
-        : 'https://shark-app-hyd9r.ondigitalocean.app'
+        : 'https://bvrfunds-dev-ulhe9.ondigitalocean.app'
 };
 
 // ===========================================

59085423746ba5ede398dc486fff97aa73875a2c
prasannanjaneyulubandaru-gif <prasannanjaneyulu.bandaru@gmail.com>
2026-02-01 18:16:13 +0530

    Update short_straddle.js

--- short_straddle.js
+++ short_straddle.js
@@ -5,7 +5,7 @@
 const STRADDLE_CONFIG = {
     backendUrl: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
         ? 'http://localhost:5000'
-        : https://bvrfunds-dev-ulhe9.ondigitalocean.app'
+        : 'https://bvrfunds-dev-ulhe9.ondigitalocean.app'
 };
 
 // State management
